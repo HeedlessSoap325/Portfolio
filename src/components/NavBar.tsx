82b02fdc1f06feec5ce7fe50ec1b4b899f231f03
HeedlessSoap325 <mathisjulian123@gmail.com>
2025-12-17 16:35:46 +0100

    Improve Language Selection

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,41 @@
 import {Link} from "react-router-dom";
-import {Language} from "../scripts/lang/translations.ts";
+import {availableLanguages, Language} from "../scripts/lang/translations.ts";
 import {useTranslation} from "../scripts/lang/TranslationProvider.tsx";
+import {ChangeEvent} from "react";
 
 export default function NavBar({setLanguage}: {setLanguage: React.Dispatch<React.SetStateAction<Language>>}) {
     const {translate} = useTranslation();
+
+    function changeLanguage(event: ChangeEvent<HTMLSelectElement>) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        if (availableLanguages.indexOf(event.target.value) !== -1) {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            setLanguage(event.target.value);
+            return;
+        }
+        setLanguage("en");
+    }
     return(
         <div id="navBar-container">
             <Link to="/Portfolio/" id="navBar-text">{translate("nav.home")}</Link>
             <nav id="navBar-nav">
                 <ul id="navBar-links">
-                    <li className="navbar-item"><Link className="navbar-link" to="/Portfolio/projects">{translate("nav.projects")}</Link></li>
-                    <li className="navbar-item"><Link className="navbar-link" to="/Portfolio/contact">{translate("nav.contact")}</Link></li>
-                    <li className="navbar-item"><Link className="navbar-link" to="/Portfolio/about">{translate("nav.about")}</Link></li>
-                    <li className="navbar-item"><button onClick={() => setLanguage("en")}>English</button></li>
-                    <li className="navbar-item"><button onClick={() => setLanguage("de")}>Deutsch</button></li>
+                    <li className="navbar-item"><Link className="navbar-link"
+                                                      to="/Portfolio/projects">{translate("nav.projects")}</Link></li>
+                    <li className="navbar-item"><Link className="navbar-link"
+                                                      to="/Portfolio/contact">{translate("nav.contact")}</Link></li>
+                    <li className="navbar-item"><Link className="navbar-link"
+                                                      to="/Portfolio/about">{translate("nav.about")}</Link></li>
+
+
+                    <select id="navBar-language-selector" onChange={(e) => changeLanguage(e)}>
+                        {availableLanguages.map(language => (
+                            <option key={language} value={language}>{translate(language)}</option>
+                        ))}
+                    </select>
+
                 </ul>
             </nav>
         </div>
--- src/scripts/lang/translations.ts
+++ src/scripts/lang/translations.ts
@@ -3,3 +3,4 @@ import langs  from "../../assets/langs.json";
 export const translations = langs;
 export type Language = keyof typeof langs
 export type TranslationKey = keyof typeof langs["en"]
+export const availableLanguages: Language[] = Object.keys(translations) as Language[]
