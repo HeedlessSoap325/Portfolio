ce127c9de11ca8a86f461798a50fe0299e95ab13
HeedlessSoap325 <mathisjulian123@gmail.com>
2026-01-07 16:18:35 +0100

    Fix About Me on Phone

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -9,7 +9,7 @@ export default function About(){
                 <div id="about-left">
                     <h3 id="about-general" className="about-header">{translate("about.general")}</h3>
                     <span>
-                        {translate("about.birthday")} <a id="about-birthday-link" href="https://www.onthisday.com/date/2008/november/20" target="_blank">20.11.2008</a>
+                        {translate("about.birthday")} <a id="about-birthday-link" href="https://www.onthisday.com/date/2008/november/20" target="_blank">{new Date("2008-11-20T10:10:10.100Z").toLocaleDateString()}</a>
                     </span>
                     <span>
                         {translate("about.home")} <a id="about-origin-link" href="https://www.google.com/maps/place/Elgg/@47.4797661,8.8169957,13z/data=!3m1!4b1!4m6!3m5!1s0x479a96ea1e3774f7:0xef4c669b47e2d556!8m2!3d47.4910721!4d8.8665461!16zL20vMGdwMGdz?entry=ttu&g_ep=EgoyMDI1MTIwNy4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D" target="_blank">Elgg, ZH</a>
@@ -41,18 +41,18 @@ export default function About(){
                             <div className="about-education-timespan">
                                 2024 - Jetzt
                             </div>
-                            <div className="about-education-description">
+                            <span className="about-education-description">
                                 Informatikmittelschule IMS, Kantonschule Büelrain ZH.
-                            </div>
+                            </span>
                         </li>
                         <li className="about-education-item">
                             <div className="about-education-timespan">
                                 2023 - 2024
                             </div>
-                            <div className="about-education-description">
-                                Wirtschaftsgymnasium, Kantonschule Büelrain ZH. <br/>
+                            <span className="about-education-description">
+                                Wirtschaftsgymnasium, Kantonschule Büelrain ZH.<br/>
                                 Nach einem Jahr in die IMS gewechselt.
-                            </div>
+                            </span>
                         </li>
                     </ul>
                 </div>
